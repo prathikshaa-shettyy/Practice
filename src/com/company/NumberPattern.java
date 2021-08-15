8e077c1394035b89a4a8ec5fbde68a4fcde10ad3
Prathiksha shetty <prathikshashetty47@gmail.com>
2021-08-16 00:03:09 +0530

    Number Pattern

---
+++ src/com/company/NumberPattern.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class NumberPattern {
+
+    public static void main(String[] args) {
+        // Pattern + Number
+        int n=5;
+//        for (int i=1; i<=n; i++) {
+//
+//            for (int j=1; j<=i; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+/* Output
+1
+12
+123
+1234
+12345
+*/
+//        for (int i=1; i<=n; i++) {
+//
+//            for (int j=i; j<=n; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+/* Output
+12345
+2345
+345
+45
+5
+//*/
+//        for (int i= 1, p=0; i<=n; i++, p++) {
+//            for (int j=1; j<=i; j++) {
+//                System.out.print(p+1);
+//            }
+//            System.out.println();
+//        }
+/* OUTPUT
+1
+22
+333
+4444
+55555
+ */
+
+
+
+
+    }
+}
